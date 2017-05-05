8d5fdcdbb306dafadefd83aadb4b20aa2e8b94f8
Ross Maniaci <roscoman1986@yahoo.com>
2017-05-04 22:03:15 -0700

    Initial Commit

---
+++ RMSpellChecker/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  RMSpellChecker
+//
+//  Created by Ross Maniaci on 5/4/17.
+//  Copyright © 2017 Lantern, Inc. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
